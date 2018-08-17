7797ac1498ba74bf2d59bc8db8784af12bae00e6
entymon <pawel.olejniczak.i@gmail.com>
2018-08-17 14:43:01 +0100

    homepage done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,28 +12,31 @@ import { makeActiveLink } from '../store/actions/MenuAction';
 })
 export default class Menu extends React.Component {
 	
-	linkHandler = (name) => {
+	_linkHandler = (name) => {
 		this.props.dispatch(makeActiveLink(name));
 	};
 	
 	render = () => {
 		return (
-			<div className="main-menu">
-				<div className="menu-item">
-					<Link onClick={() => this.linkHandler(ROUTES.HOME)} className="company-name" to="/home">Smooth Code Line</Link>
+			<div className="main-menu-container">
+				<div className="main-menu">
+					<div className="menu-item">
+						<Link onClick={() => this._linkHandler(ROUTES.HOME)} className="company-name" to="/home">Smooth Code Line</Link>
+					</div>
+					<div className="menu-item">
+						<Link onClick={() => this._linkHandler(ROUTES.PORTFOLIO)} className={`link ${this.props.link === ROUTES.PORTFOLIO && 'active'}`} to="/portfolio">What I have done</Link>
+					</div>
+					<div className="menu-item">
+						<Link onClick={() => this._linkHandler(ROUTES.WORK_RANGE)} className={`link ${this.props.link === ROUTES.WORK_RANGE && 'active'}`} to="/work-range">How I can help you</Link>
+					</div>
+					<div className="menu-item">
+						<Link onClick={() => this._linkHandler(ROUTES.CV)} className={`link ${this.props.link === ROUTES.CV && 'active'}`} to="/cv">My CV</Link>
+					</div>
+					<div className="menu-item">
+						<Link onClick={() => this._linkHandler(ROUTES.BLOG)} className={`link ${this.props.link === ROUTES.BLOG && 'active'}`} to="/blog">Stories</Link>
+					</div>
 				</div>
-				<div className="menu-item">
-					<Link onClick={() => this.linkHandler(ROUTES.PORTFOLIO)} className={`link ${this.props.link === ROUTES.PORTFOLIO && 'active'}`} to="/portfolio">What I have done</Link>
-				</div>
-				<div className="menu-item">
-					<Link onClick={() => this.linkHandler(ROUTES.WORK_RANGE)} className={`link ${this.props.link === ROUTES.WORK_RANGE && 'active'}`} to="/work-range">How I can help you</Link>
-				</div>
-				<div className="menu-item">
-					<Link onClick={() => this.linkHandler(ROUTES.CV)} className={`link ${this.props.link === ROUTES.CV && 'active'}`} to="/cv">My CV</Link>
-				</div>
-				<div className="menu-item">
-					<Link onClick={() => this.linkHandler(ROUTES.BLOG)} className={`link ${this.props.link === ROUTES.BLOG && 'active'}`} to="/blog">Blog</Link>
-				</div>
+				<div className='show-block'/>
 			</div>
 		);
 	}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import './Home.css'
+import ProgrammerImage from '../assets/images/programmer.png'
 
 export default class Home extends React.Component {
-
+	
+	state = {
+		containerHeight: window.innerHeight
+	};
+	
+	_handleWindowResize = () => {
+		this.setState({
+			containerHeight: window.innerHeight
+		});
+	};
+	
+	componentDidMount = () => {
+		window.addEventListener('resize', this._handleWindowResize);
+	};
+	
+	componentWillUnmount() {
+		window.removeEventListener('resize', this._handleWindowResize);
+	}
+	
 	render = () => {
-		return (<div>Home page</div>);
+		return (
+			<div className="container home-container">
+				<div className="story-container">
+					<div className="story">
+						<p className='text'>Hi Everyone!</p>
+						<p className='text'>I am a web developer</p>
+						<p className='text'>and</p>
+						<p className='text'>I love what I am doing.</p>
+					</div>
+					<div className="image">
+						<img src={ProgrammerImage} alt=""/>
+					</div>
+				</div>
+				
+				<hr className="divider"/>
+				
+				<div className="experience-container">
+					test
+				</div>
+			</div>
+		);
 	}
 }
